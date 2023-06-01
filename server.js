ba91c2cef422628cfc136919bd1c393607f337bf
Hadi Rastin <HadiRastin88@gmail.com>
2023-05-31 18:49:34 -0700

    5th commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send(<h1>This is first version</h1>);
+  res.send("This is 6th try");
 });
 
 app.listen(PORT, HOST, () => {
