0020f470ee8dfae25e762ec6c8fa0617cbe3855a
Hadi Rastin <HadiRastin88@gmail.com>
2023-05-31 18:02:10 -0700

    second commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('My Final Changes');
+  res.send('This is hadi rastin');
 });
 
 app.listen(PORT, HOST, () => {
